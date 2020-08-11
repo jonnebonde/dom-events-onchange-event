9a8ed8ef7de2a4c8d338e9c3d6ba9b9b2e2d3bcb
cnnrbrn <connorbobrien@gmail.com>
2020-08-12 00:20:16 +0200

    First commit

---
+++ js/script.js
@@ -0,0 +1,20 @@
+const dropdown = document.querySelector("select");
+const circlesContainer = document.querySelector(".circles");
+
+dropdown.onchange = function () {
+    console.log(event.target.value);
+
+    const selectedValue = this.value;
+
+    circlesContainer.innerHTML = "";
+
+    for (let i = 1; i <= selectedValue; i++) {
+        let className = "";
+
+        if (i % 2 === 0) {
+            className = "highlight";
+        }
+
+        circlesContainer.innerHTML += `<div class="circle ${className}">${i}</div>`;
+    }
+};
