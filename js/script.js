9ec63c7b317fc862a75b759a4f6f585eac3d0bea
jonnebonde <jonnebonde@gmail.com>
2022-02-03 10:35:54 +0100

    enter

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-const dropdown = document.querySelector("select");
+/* const dropdown = document.querySelector("select");
 const circlesContainer = document.querySelector(".circles");
 
 dropdown.onchange = function() {
@@ -20,4 +20,34 @@ dropdown.onchange = function() {
         circlesContainer.innerHTML += `<div class="circle ${className}">${i}</div>`;
 
     }
+}; */
+
+
+
+// setTimeout example
+
+const dropdown = document.querySelector("select");
+const circlesContainer = document.querySelector(".circles");
+
+dropdown.onchange = function() {
+    console.log(event.target.value);
+
+    const selectedValue = this.value;
+
+    circlesContainer.innerHTML = `<div class="spinner"></div>`;
+
+    setTimeout(function(){
+        circlesContainer.innerHTML = "";
+
+        for(let i = 1; i <= selectedValue; i++) {
+            let className = "";
+    
+            if(i % 2 === 1) {
+                className = "highlight";
+            }
+    
+            circlesContainer.innerHTML += `<div class="circle ${className}">${i}</div>`;
+
+        }
+    }, 1000);
 };
